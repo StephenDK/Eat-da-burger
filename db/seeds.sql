066cdd6993e4a9aebc0ce8c346bf3074c51b15e8
StephenDK <stephendklein08@gmail.com>
2018-02-19 15:40:20 -0800

    mysql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy chicken burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double bacon burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double double animal style", 0);
