3199f6c23244ff5ea9df2d56144d58163b5e6a0d
StephenDK <stephendklein08@gmail.com>
2018-02-19 19:24:12 -0800

    orm all function

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,4 @@
 // require the mysql connection 
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 // Object relational mapper
-var orm = {
-    SelectAll: function(cb) {
-        var queryString = "SELECT * FROM burgers;";
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    }
-
-};
